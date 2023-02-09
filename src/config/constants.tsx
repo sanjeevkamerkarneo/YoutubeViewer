abb3645320654a545ce450f443198a354cc25fb8
Sanjeev Kamerkar <sanjeev.kamerkar@neosoftmail.com>
2023-02-09 14:04:17 +0530

    changes in git ignore

--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -1,1 +1,4 @@
-export const API_KEY = 'AIzaSyBZq4Ez5kKMTXITuTpaDxlu98MDr4g05fY';
+// export const API_KEY = 'AIzaSyBZq4Ez5kKMTXITuTpaDxlu98MDr4g05fY';
+export const API_KEY = 'AIzaSyD8ZAgc13AJzzF4WP_tYirWJ_X7BVHnto0';
+
+// AIzaSyD8ZAgc13AJzzF4WP_tYirWJ_X7BVHnto0
--- src/service/youtube.service.tsx
+++ src/service/youtube.service.tsx
@@ -2,7 +2,13 @@ import { axiosInstance } from './axios';
 import { GetYouTubeComments } from '../config/api-endpoint';
 import { API_KEY } from '../config/constants';
 
-export const    getPrimaryComments = async (videoId: string) => {
+export const getPrimaryComments = async (videoId: string) => {
     // return axiosInstance.get
-    return await axiosInstance.get(GetYouTubeComments, { params: { videoId: videoId, key: API_KEY } });
+    var params = {
+        part: 'id, snippet, replies',
+        key: API_KEY,
+        videoId: videoId,
+    }
+
+    return await axiosInstance.get(GetYouTubeComments, { params: params });
 }
